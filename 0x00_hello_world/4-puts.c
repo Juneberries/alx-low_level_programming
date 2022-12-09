05e48c2bd2029a4dc7e203f8a9458fcc4cf56e3d
juneberries <junekilatya@gmail.com>
2022-12-08 16:02:35 -0800

    task 4

---
+++ 0x00_hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main- Entry point
+ *
+ *Description: printscustom message followed by new line
+ *
+ *return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
