824145a75a0a007ebf4074a9696fb8154144fd36
juneberries <junekilatya@gmail.com>
2022-12-13 12:58:29 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printsa given phrase
+ * Return: 0 if exited right & non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
