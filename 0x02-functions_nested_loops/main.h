a0ef404575c9a6e0160623e6a66d0e6ad1e3bd01
juneberries <junekilatya@gmail.com>
2022-12-13 14:22:50 -0800

    task 10

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,7 +10,7 @@ int _abs(int t);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 int _putchar(char c);
 
