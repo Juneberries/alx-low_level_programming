e5f795a571613fc08de1bfce52776d2a5c1c5068
juneberries <junekilatya@gmail.com>
2022-12-13 13:36:18 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#Include <unistd.h>
+/**
+ * main - prints a phrase followed by a new line, to the standard error
+ * Return: Always 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
