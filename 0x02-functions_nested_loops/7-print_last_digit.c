0b229d17f143dd2f1439bd313aa4449adc0eb2bd
juneberries <junekilatya@gmail.com>
2022-12-13 11:08:17 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @n: ht enumber
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	if (ld < 0)
+		ld *= -1;
+	_putchar(ld + '0');
+	return (0);
+}
