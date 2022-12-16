a7e81b271486163815703968334f94b0982f520f
juneberries <junekilatya@gmail.com>
2022-12-16 14:49:42 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_number - prints numbers between 0 and 9
+ * (neither 2 nor 4)
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		if (ch != 50 && ch != 52)
+			_putchar(ch);
+	}
+	_putchar('\n');
+}
