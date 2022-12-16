72d43c1053136a117a38e673bf18b4aa2bfdd781
juneberries <junekilatya@gmail.com>
2022-12-16 14:20:39 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * file - main.h
+ * Author: Juneberries
+ * Desc: Header contains prototypes for all functions 
+ * used in 0x04-more_functions_nested_loops directory
+ */
+int _putchar(char c);
+
+
+
+
+
+#endif
