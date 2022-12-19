51820b7dd166384b35d302503eed156a9f09448b
juneberries <junekilatya@gmail.com>
2022-12-19 14:37:43 -0800

    task 2 redo (1)

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,14 @@
 /**
  * _strlen - returns the length of a string
  * @s: input spring
- * Return: length of string
+ * Return: void
  */
 int _strlen(char *s)
 {
-	int index;
+	int i = 0;
 
-	for (index = 0; s[index] != '\0' ; index++)
+	while (s[i])
+		i++;
 
-	return (index);
+	return (i);
 }
