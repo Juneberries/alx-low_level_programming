1ac69bf9d2720d6d88fdabee652e1dbffc0c0755
juneberries <junekilatya@gmail.com>
2022-12-19 15:29:44 -0800

    task 8 redo

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,13 +8,16 @@
  */
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i < n - 1)
+		if (i != n - 1)
+		{
 			printf(",");
+			printf(" ");
+		}
 	}
 	printf("\n");
 }
