ba8f12bef444cf952de90a67c08e94846d4abe9e
juneberries <junekilatya@gmail.com>
2022-12-19 14:27:22 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: input spring
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' ; index++)
+
+		return (index);
+}
