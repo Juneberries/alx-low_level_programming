1be86347dd20c8f5b86ee3af519c6314a875dc0b
juneberries <junekilatya@gmail.com>
2022-12-19 15:19:21 -0800

    task 6 redo (3)

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,9 +6,8 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
@@ -17,5 +16,5 @@ void puts2(char *str)
 		}
 		i++;
 	}
-	_pucthar('\n');
+	_putchar('\n');
 }
