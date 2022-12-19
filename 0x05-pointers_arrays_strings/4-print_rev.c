dc65d52abcb213fbb9c65ab824a5f804ad3544f5
juneberries <junekilatya@gmail.com>
2022-12-19 14:41:03 -0800

    task 4 redo

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,13 +5,13 @@
  */
 void print_rev(char *s)
 {
-	int index;
+	int i = 0;
 
-	for (index = 0; s[index] != '\0'; index++)
-
-		for (index = index - 1; s[index] != '\0'; index++)
-		{
-			_putchar(s[index]);
-		}
+	while (s[i])
+		i++;
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
 	_putchar('\n');
 }
