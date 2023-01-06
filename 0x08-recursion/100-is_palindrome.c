a885e1b11419be354dc04212a16768d8da4fd0e7
juneberries <junekilatya@gmail.com>
2023-01-06 14:39:07 -0800

    task 7 redo

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,43 +1,52 @@
-#include <stdio.h>
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: the string to count
- * Return: length of the string
- */
+* _strlen_recursion - return length of string
+* @s: character string
+* Return: string length
+**/
 int _strlen_recursion(char *s)
 {
-	if (*s)
-	{
-		s++;
-		return (1 + _strlen_recursion(s));
-	}
-	return (0);
-}
-/**
- * checker - helper function for is_palindrome
- * @str: the string
- * @len: length of string
- * @count: counter of recursion
- * Return: 1 if string is a palindrome, 0 if it is not.
- */
-int checker(char *str, int len, int count)
+int i = 0;
+
+if (*s == '\0')
+return (0);
+else
 {
-	if (count >= len)
-		return (1);
-	if (str[len] == str[count])
-		return (checker(str, len - 1, count + 1));
-	return (0);
+i = _strlen_recursion(s + 1);
+i++;
 }
+return (i);
+}
+
 /**
- * is_palindrome - checks if the string is a palindrome
- * @s: the string to check
- * Return: 1 if string is a palindrome, 0 if it is not.
- */
+* _scan - scan string
+* @c: char
+* @d: char
+* Return: char
+**/
+int _scan(char *c, char *d)
+{
+if (*c != *d)
+return (0);
+else if (c >= d)
+return (1);
+else
+return (_scan(c + 1, d - 1));
+}
+
+/**
+* is_palindrome - checks if string is a palindrome
+* @s: pointer to a string
+* Return: 1 if palindrome and 0 if not
+**/
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s);
-	int count = 0;
+int i = _strlen_recursion(s);
+char *c = s;
+char *d = s + (i - 1);
 
-	return (checker(s, len - 1, count));
+if (i == 0)
+return (1);
+return (_scan(c, d));
 }
