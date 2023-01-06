a461b8f12b86ecd913b9eebb9248522f09c7fec4
juneberries <junekilatya@gmail.com>
2023-01-06 12:41:51 -0800

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+unsigned int factorial (unsigned int n)
+{
+	if (n <= 1)
+		return 1;
+	return n * factorial (n - 1);
+}
+int main ()
+{
+	printf("%i\n", factorial (4));
+	return 0;
+}
+
