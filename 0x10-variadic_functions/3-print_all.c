4249db54c7d24c3a32ec2a26a91ac85b1a9356e1
juneberries <junekilatya@gmail.com>
2023-01-20 12:52:06 -0800

    task 3 redo

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,44 +8,42 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list valist;
-	unsigned int i = 0, j, c = 0;
-	char *str;
-	const char t_arg[] = "cifs";
+	int i = 0;
+	char *str, *sep = "";
 
-	va_start(valist, format);
-	while (format && format[i])
+	va_list list;
+
+	va_list(list, format);
+
+	if (format)
 	{
-		j = 0;
-		while (t_arg[j])
+		while (format[i])
 		{
-			if (format[i] == t_arg[j] && c)
+			switch (format[i])
 			{
-				printf(", ");
-				break;
-			} j++;
-		}
-		switch (format[i])
-		{
-			case 'c':
-			printf("%c", va_arg(valist, int)), c = 1;
-			break;
-			case 'i':
-			printf("%d", va_arg(valist, int)), c = 1;
-			break;
-			case 'f':
-			printf("%f", va_arg(valist, double)), c = 1;
-			break;
-			case 's':
-			str = va_arg(valis, chat *), c = 1;
-			if (!str)
-			{
-				printf("(nil)");
-				break;
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
 			}
-			printf("%s", str);
-			break;
-		} i++;
+			sep = ", ";
+			i++;
+		}
 	}
-	printf('\n'), va_end(valist);
+	printf("\n");
+	va_end(list);
 }
