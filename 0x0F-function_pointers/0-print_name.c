64118925462026710e2fcf872a19c6cd16e4a9ba
juneberries <junekilatya@gmail.com>
2023-01-20 13:33:02 -0800

    task 0 redo

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,10 @@
  * print_name - prints a name
  * @name: name to print
  * @f: pointer to the printing function
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name)
+	if (name && f)
+		f(name);
 }
